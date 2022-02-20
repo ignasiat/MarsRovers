45111051ae062f73e7258e77a1026d9e58cff964
Ignasi Amargós <ignasi.amargos@gmail.com>
2022-02-20 11:28:55 +0100

    Create component and styles RoverExpeditiion

---
+++ src/components/RoverExpeditionComponent/index.tsx
@@ -0,0 +1,90 @@
+import React from 'react';
+import barrier from '../../assets/barrier.png';
+import layers from '../../assets/layers.png';
+import marsRover from '../../assets/mars-rover.png';
+import {
+  ORIENTATIONS_TEXT,
+  TERRAIN_AVAILABLE_TEXT,
+  TERRAIN_OBSTACLE,
+  TERRAIN_OBSTACLE_TEXT,
+  TERRAIN_ROVER,
+  TERRAIN_ROVER_TEXT
+} from '../../constants';
+import {
+  Configuration,
+  RoverExpedition,
+  TerrainElement,
+  TerrainLayer
+} from '../../types';
+import './styles.scss';
+
+const RoverExpeditionComponent = ({
+  expedition,
+  configuration
+}: { expedition: RoverExpedition | undefined, configuration: Configuration }) => (
+
+  <div className="rover-container">
+    <h1> Rover Expedition</h1>
+    <div>
+      {expedition?.terrain.map((firstLayer: TerrainLayer, indexY: number) => (
+        <div className="terrain-layer">
+          {firstLayer.map((element: TerrainElement, indexX: number) => {
+            switch (element) {
+              case TERRAIN_OBSTACLE:
+                return (
+                  <img src={barrier} alt={TERRAIN_OBSTACLE_TEXT} />);
+              case TERRAIN_ROVER:
+                return indexX === configuration.xOrigin - 1
+                  && indexY === configuration.yOrigin - 1 ? (
+                  <img src={marsRover} alt={TERRAIN_ROVER_TEXT} className="terrain-origin-rover" />
+                ) : (
+                  <img src={marsRover} alt={TERRAIN_ROVER_TEXT} />
+                );
+              default:
+                return (
+                  <img src={layers} alt={TERRAIN_AVAILABLE_TEXT} />);
+            }
+          })}
+        </div>))}
+    </div>
+    <table>
+      <thead>
+        <tr>
+          <th colSpan={2}>
+            Mission summary
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Origin</td>
+          <td className="terrain-origin-rover">
+            {configuration.xOrigin}
+            ,
+            {configuration.yOrigin}
+          </td>
+        </tr>
+        <tr>
+          <td>Orientation:</td>
+          <td>
+            {ORIENTATIONS_TEXT[configuration.directionOrigin]}
+          </td>
+        </tr>
+        <tr>
+          <td>Sequence sended:</td>
+          <td>
+            {configuration.commands.split('').join(',')}
+          </td>
+        </tr>
+        <tr>
+          <td>Sequence executed:</td>
+          <td>
+            {expedition?.movementsDone.join(',')}
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+);
+
+export default RoverExpeditionComponent;
